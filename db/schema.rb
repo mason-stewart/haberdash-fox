568f972130ec9b09d9730e626c861c10f66e58a5
Mason Stewart <masondesu@gmail.com>
2012-10-09 13:20:49 -0400

    Ran Item/Collection association migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121009170244) do
+ActiveRecord::Schema.define(:version => 20121009171932) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -59,8 +59,9 @@ ActiveRecord::Schema.define(:version => 20121009170244) do
     t.string   "photo"
     t.integer  "price"
     t.string   "url"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.integer  "collection_id"
   end
 
 end

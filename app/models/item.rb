b04b89489ba5c1c6d208a76c5dec7ad0ff530b30
Mason Stewart <masondesu@gmail.com>
2012-11-04 10:30:28 -0500

    Started fleshing out Item model.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,22 @@
 class Item < ActiveRecord::Base
-  attr_accessible :description, :photo, :price, :title, :url
+  before_create :fetch_from_etsy
+
+  def fetch_from_etsy
+    item = Etsy::Listing.find(self.etsy_id)
+    self.description = item.description
+    self.title = item.title
+    self.price = item.price
+    self.url = item.url
+    self.etsy_id = item.id
+    self.photos = item.images
+  end
+
+  serialize :photos
+
+  attr_accessible :description, :photos, :price, :title, :url, :etsy_id, :collection_id
+
+  validates :etsy_id, :presence => true
+  validates :etsy_id, :uniqueness => true
 
   belongs_to :collection
 end

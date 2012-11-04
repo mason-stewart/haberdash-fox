199087d7d48cc8f48d11c2bbbe4d64c0de94906f
Mason Stewart <masondesu@gmail.com>
2012-11-04 10:52:05 -0500

    Item model now saves Photos.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,29 @@
 class Item < ActiveRecord::Base
   before_create :fetch_from_etsy
+  after_create :save_photos
 
   def fetch_from_etsy
-    item = Etsy::Listing.find(self.etsy_id)
-    self.description = item.description
-    self.title = item.title
-    self.price = item.price
-    self.url = item.url
-    self.etsy_id = item.id
-    self.photos = item.images
+    @item = Etsy::Listing.find(self.etsy_id)
+    self.description = @item.description
+    self.title = @item.title
+    self.price = @item.price
+    self.url = @item.url
+    self.etsy_id = @item.id
   end
 
-  serialize :photos
+  def save_photos
+    @item.images.each do |image|
+      self.photos.create(:attrs => image.result)
+    end
+  end
 
-  attr_accessible :description, :photos, :price, :title, :url, :etsy_id, :collection_id
+
+  attr_accessible :description, :price, :title, :url, :etsy_id, :collection_id
 
   validates :etsy_id, :presence => true
   validates :etsy_id, :uniqueness => true
 
   belongs_to :collection
+  has_many :photos, :dependent => :destroy
+
 end

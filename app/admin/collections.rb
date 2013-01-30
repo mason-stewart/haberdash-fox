b6f57a50c5fdafce6a4c1a417df2c44288dc4acd
Mason Stewart <masondesu@gmail.com>
2013-01-30 16:05:31 -0500

    Made Collections drag-and-droppable in active admin

--- app/admin/collections.rb
+++ app/admin/collections.rb
@@ -1,2 +1,18 @@
 ActiveAdmin.register Collection do
+  config.sort_order = 'position_asc'
+
+  index do
+    column :title
+    default_actions
+  end
+
+  # This action is called by javascript when you drag and drop a column
+  # It iterates through the collection and sets the new position based on the
+  # order that jQuery submitted them
+  collection_action :sort, :method => :post do
+    params[:collection].each_with_index do |id, index|
+      Collection.update_all(['position=?', index+1], ['id=?', id])
+    end
+    render :nothing => true
+  end
 end

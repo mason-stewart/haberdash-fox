f064b5ec107bbf62af1ed7e269ac383dac809546
Mason Stewart <masondesu@gmail.com>
2013-02-14 10:34:54 -0500

    Use 3 unicorn workers per dyno, instead of 4

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 preload_app true
-worker_processes 4 # amount of unicorn workers to spin up
+worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) and

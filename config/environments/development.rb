0b9a1c1041d99cd89e4208712cb917d33840dd70
Mason Stewart <masondesu@gmail.com>
2013-04-19 16:36:30 -0400

    Added bullet config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,13 @@ HaberdashFox::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
+
 end

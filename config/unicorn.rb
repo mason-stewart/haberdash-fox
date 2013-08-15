035458226a8465450ca7060681096926d502c718
Mason Stewart <masondesu@gmail.com>
2013-08-15 12:01:02 -0400

    [TEMPORARY] unicorn worker timeout bumped from 30 to 60

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 preload_app true
 worker_processes 3 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
+timeout 60         # restarts workers that hang for 60 seconds
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
   ActiveRecord::Base.establish_connection

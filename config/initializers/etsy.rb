e4fd4fd7e4db138912bb9926924a8e05b975957e
Mason Stewart <masondesu@gmail.com>
2014-02-16 11:12:50 -0500

    Specify HTTPS in Etsy config

--- config/initializers/etsy.rb
+++ config/initializers/etsy.rb
@@ -1,3 +1,6 @@
+# New (late 2013ish) Etsy API keys are now required
+# to use HTTPS. Older keys can still get away with HTTP
+Etsy.protocol = 'https'
 Etsy.environment = :production
 Etsy.api_key = ENV['ETSY_API_KEY']
 Etsy.api_secret = ENV['ETSY_API_SECRET']
